c44496cd78e6fffa6241253a39f06e4a6cc37847
Jose <jose.antonio.umeres@gmail.com>
2022-11-04 21:03:13 -0500

    Added description to my label component

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,9 +1,23 @@
 import './myLabel.css';
 
-export const MyLabel = () => {
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar en la etiqueta
+    */
+    label?: string;
+    /**
+     * Este es el tamaño de la etiqueta
+    */
+    size: 'normal'|'h1'|'h2'|'h3';
+}
+
+export const MyLabel = ({
+    label = 'No Label',
+    size = 'normal',
+}: MyLabelProps) => {
     return (
-        <span>
-            Hola
+        <span className={`${size}`}>
+            { label }
         </span>
     )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,16 +1,20 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { MyLabel } from "../../components/MyLabel";
 
 // con title se dice a storybook que se coloque en el directorio 'UI' el componente
 export default {
     title: 'UI/MyLabel',
     component: MyLabel
-}
+} as ComponentMeta<typeof MyLabel>
 
-// sirve para que se pueda mostrar
-const Template = () => <MyLabel />
+// Template sirve para que se pueda mostrar
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
 
 // para mostrar las páginas de la historia
 // con el bind rompe la referencia y crea una compia de Template
 export const Basic = Template.bind({});
+Basic.args = {
+    size: 'normal',
+}
 export const AllCaps = Template.bind({});
 export const Secondary = Template.bind({});
